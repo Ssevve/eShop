e674602cb53315f3014f8ea0c20fc3cf0815ca17
Seweryn Świrgoń <ssevve@gmail.com>
2023-04-08 12:55:51 +0200

    Style product card; Add currency conversion;

--- src/features/products/components/ProductCard/ProductCard.tsx
+++ src/features/products/components/ProductCard/ProductCard.tsx
@@ -8,19 +8,34 @@ interface ProductCardProps {
 function ProductCard({ product }: ProductCardProps) {
   const isDiscount = product.discountPrice < product.price;
 
+  const convertInrToUsDollars = (price: number): number => {
+    return parseFloat((price * 0.012214).toFixed(2));
+  };
+
+  const dollarPrice = convertInrToUsDollars(product.price);
+  const discountDollarPrice = convertInrToUsDollars(product.discountPrice);
+
   return (
-    <div className="border">
+    <div className="flex w-64 flex-col gap-2 p-3">
       <img src={product.imageUrl} alt={product.productName} />
-      <h3>{product.productName}</h3>
-      <span>{product.brand}</span>
-      <span>In stock: {product.quantity}</span>
-      <div>
-        <span className={cx(isDiscount ? 'text-md line-through' : 'text-xl')}>
-          {product.price}
-        </span>
-        {isDiscount && (
-          <span className="text-xl text-red-500">{product.discountPrice}</span>
-        )}
+      <div className="flex justify-between">
+        <div className="flex flex-col gap-1">
+          <h3>{product.productName}</h3>
+          <span className="text-sm">Qty: {product.quantity}</span>
+          <span className="text-xs">{product.brand}</span>
+        </div>
+        <div className="gap-42 flex items-center justify-end gap-2">
+          <span
+            className={cx(isDiscount ? 'text-lg line-through' : 'text-2xl')}
+          >
+            ${dollarPrice}
+          </span>
+          {isDiscount && (
+            <span className="text-2xl text-red-500">
+              ${discountDollarPrice}
+            </span>
+          )}
+        </div>
       </div>
       <button
         className="rounded-sm bg-green-600 p-2 font-bold text-white hover:bg-green-500"
