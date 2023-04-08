a2ac288ea4f72ba364b4934db3e53d39383f0154
Seweryn Świrgoń <ssevve@gmail.com>
2023-04-08 10:08:21 +0200

    Remove not needed types

--- src/features/products/productsSlice.ts
+++ src/features/products/productsSlice.ts
@@ -1,5 +1,5 @@
 /* eslint-disable no-param-reassign */
-import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
+import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import { DocumentData, getDocs } from 'firebase/firestore';
 import { productsCollection } from 'firebaseConfig';
 import ProductsState from 'types/ProductsState';
@@ -22,16 +22,13 @@ const productsSlice = createSlice({
       .addCase(getProducts.pending, (state) => {
         state.status = 'PENDING';
       })
-      .addCase(
-        getProducts.fulfilled,
-        (state, action: PayloadAction<DocumentData>) => {
-          state.status = 'SUCCESS';
-          state.products = action.payload.docs.map((doc: DocumentData) => ({
-            ...doc.data(),
-            id: doc.id,
-          }));
-        }
-      )
+      .addCase(getProducts.fulfilled, (state, action) => {
+        state.status = 'SUCCESS';
+        state.products = action.payload.docs.map((doc: DocumentData) => ({
+          ...doc.data(),
+          id: doc.id,
+        }));
+      })
       .addCase(getProducts.rejected, (state) => {
         state.status = 'ERROR';
       });
