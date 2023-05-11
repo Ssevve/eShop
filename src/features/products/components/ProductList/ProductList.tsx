81ebe4fb02bef89cd48088ba082b3b99eb7f32a0
Seweryn Świrgoń <ssevve@gmail.com>
2023-05-11 10:52:30 +0200

    Make product list 2 cols on small devices

--- src/features/products/components/ProductList/ProductList.tsx
+++ src/features/products/components/ProductList/ProductList.tsx
@@ -9,7 +9,7 @@ interface ProductListProps {
 function ProductList({ products }: ProductListProps) {
   return (
     <div className="w-full">
-      <ul className="grid grid-cols-1 gap-8 md:grid-cols-2 lg:grid-cols-5">
+      <ul className="grid grid-cols-1 gap-8 sm:grid-cols-2 lg:grid-cols-5">
         {products.map((product) => (
           <li key={product?._id}>
             <ProductCard product={product} />
