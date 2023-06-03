0b1e38188c428f61ada96fba4b69e068df581c1e
Seweryn Świrgoń <ssevve@gmail.com>
2023-06-03 13:09:16 +0200

    Start LoginForm test rewrite

--- src/pages/Login/components/LoginForm/LoginForm.test.tsx
+++ src/pages/Login/components/LoginForm/LoginForm.test.tsx
@@ -1,70 +1,69 @@
+// @vitest-environment jsdom
+import { vi } from 'vitest';
 import { BrowserRouter } from 'react-router-dom';
-import { screen } from '@testing-library/react';
-import { describe, test, expect } from 'vitest';
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import renderWithProviders from 'utils/renderWithProviders';
 import LoginForm from '.';
 
-describe('Login form', () => {
-  test('Renders a form', () => {
+vi.mock('firebase/auth', () => {
+  return {
+    getAuth: vi.fn(),
+    signInWithEmailAndPassword: vi.fn().mockReturnValue({}),
+  };
+});
+
+describe('LoginForm component', () => {
+  beforeEach(() => {
     renderWithProviders(
       <BrowserRouter>
         <LoginForm />
       </BrowserRouter>
     );
-    expect(screen.getByRole('form')).toBeInTheDocument();
   });
 
-  test('Renders logo', () => {
-    renderWithProviders(
-      <BrowserRouter>
-        <LoginForm />
-      </BrowserRouter>
-    );
-    expect(screen.getByRole('note', { name: /eshop/i })).toBeInTheDocument();
+  it('should show error message if email is not valid', async () => {
+    const userCredentials = {
+      email: 'bademail',
+      password: 'testpassword',
+    };
+    const user = userEvent.setup();
+    await user.type(screen.getByRole('textbox', { name: 'Email' }), userCredentials.email);
+    await user.type(screen.getByLabelText('Password'), userCredentials.password);
+    await user.click(screen.getByRole('button', { name: 'Log in' }));
+
+    expect(signInWithEmailAndPassword).not.toHaveBeenCalled();
+    expect(screen.getByText('Invalid email')).toBeInTheDocument();
   });
 
-  test('Renders an email input', () => {
-    renderWithProviders(
-      <BrowserRouter>
-        <LoginForm />
-      </BrowserRouter>
-    );
-    expect(screen.getByRole('textbox', { name: /email/i })).toBeInTheDocument();
+  it('should show error message when password is too short', async () => {
+    const userCredentials = {
+      email: 'user@email.com',
+      password: '2',
+    };
+    const user = userEvent.setup();
+    await user.type(screen.getByRole('textbox', { name: 'Email' }), userCredentials.email);
+    await user.type(screen.getByLabelText('Password'), userCredentials.password);
+    await user.click(screen.getByRole('button', { name: 'Log in' }));
+
+    expect(signInWithEmailAndPassword).not.toHaveBeenCalled();
+    expect(screen.getByText('Minimum password length is 6')).toBeInTheDocument();
   });
 
-  test('Renders a password input', () => {
-    renderWithProviders(
-      <BrowserRouter>
-        <LoginForm />
-      </BrowserRouter>
-    );
-    expect(screen.getByLabelText(/password/i)).toBeInTheDocument();
-  });
+  it('should show error messages when user was not found', async () => {
+    const userCredentials = {
+      email: 'user@email.com',
+      password: 'testpassword',
+    };
+    const user = userEvent.setup();
+    await user.type(screen.getByRole('textbox', { name: 'Email' }), userCredentials.email);
+    await user.type(screen.getByLabelText('Password'), userCredentials.password);
+    await user.click(screen.getByRole('button', { name: 'Log in' }));
 
-  test('Renders a log in button', () => {
-    renderWithProviders(
-      <BrowserRouter>
-        <LoginForm />
-      </BrowserRouter>
-    );
-    expect(screen.getByRole('button', { name: /log in/i })).toBeInTheDocument();
-  });
-
-  test('Renders a footer text', () => {
-    renderWithProviders(
-      <BrowserRouter>
-        <LoginForm />
-      </BrowserRouter>
-    );
-    expect(screen.getByText(/need an account?/i)).toBeInTheDocument();
-  });
-
-  test('Renders a register page link', () => {
-    renderWithProviders(
-      <BrowserRouter>
-        <LoginForm />
-      </BrowserRouter>
-    );
-    expect(screen.getByRole('link', { name: /register/i })).toBeInTheDocument();
+    waitFor(() => {
+      const errors = screen.getAllByText('Invalid email or password');
+      expect(errors).toHaveLength(2);
+    });
   });
 });
