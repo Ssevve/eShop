63d6b65b76e3969da91078768616ceb45c60528f
Seweryn Świrgoń <ssevve@gmail.com>
2023-06-15 11:22:39 +0200

    Make ratingsCount optional; Add optional size prop

--- src/components/common/StarRating/StarRating.test.tsx
+++ src/components/common/StarRating/StarRating.test.tsx
@@ -5,27 +5,30 @@ import StarRating from '.';
 
 describe('StarRating component', () => {
   it('should render correct amount of stars', async () => {
-    renderWithProviders(<StarRating rating={2} ratingsCount={3} />);
-    expect(await screen.findAllByTitle('star')).toHaveLength(MAX_PRODUCT_RATING);
+    const rating = 2;
+    renderWithProviders(<StarRating rating={rating} />);
+    expect(await screen.findAllByTitle(`${rating}/${MAX_PRODUCT_RATING}`)).toHaveLength(
+      MAX_PRODUCT_RATING
+    );
   });
 
   it('should render correct amount of gold stars', async () => {
     const rating = 2;
-    renderWithProviders(<StarRating rating={rating} ratingsCount={3} />);
-    const stars = await screen.findAllByRole('img', { name: 'star' });
+    renderWithProviders(<StarRating rating={rating} />);
+    const stars = await screen.findAllByRole('img', { name: `${rating}/${MAX_PRODUCT_RATING}` });
     const goldStars = stars.filter((star) => star.getAttribute('fill') === 'gold');
     expect(goldStars).toHaveLength(rating);
   });
 
   it('should render correct amount of not filled stars', async () => {
     const rating = 2;
-    renderWithProviders(<StarRating rating={rating} ratingsCount={3} />);
-    const stars = await screen.findAllByRole('img', { name: 'star' });
+    renderWithProviders(<StarRating rating={rating} />);
+    const stars = await screen.findAllByRole('img', { name: `${rating}/${MAX_PRODUCT_RATING}` });
     const transparentStars = stars.filter((star) => star.getAttribute('fill') === 'transparent');
     expect(transparentStars).toHaveLength(MAX_PRODUCT_RATING - rating);
   });
 
-  it('should render correct amount of ratingsCount', async () => {
+  it('should render ratings count if it is provided', async () => {
     const ratingsCount = 3;
     renderWithProviders(<StarRating rating={2} ratingsCount={ratingsCount} />);
     expect(screen.getByText(`(${ratingsCount} ratings)`)).toBeInTheDocument();
