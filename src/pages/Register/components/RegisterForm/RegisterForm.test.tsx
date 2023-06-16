69149b14cc39a9fb9f0349fe5e8e0bb194c109d1
Seweryn Świrgoń <ssevve@gmail.com>
2023-06-16 18:32:52 +0200

    Add ErrorBox for server error; Change form mode to onBlur

--- src/pages/Register/components/RegisterForm/RegisterForm.test.tsx
+++ src/pages/Register/components/RegisterForm/RegisterForm.test.tsx
@@ -2,7 +2,7 @@
 
 import 'mocks/firebase';
 import { BrowserRouter } from 'react-router-dom';
-import { screen } from '@testing-library/react';
+import { fireEvent, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import renderWithProviders from 'utils/renderWithProviders';
 import { MIN_PASSWORD_LENGTH } from 'lib/constants';
@@ -18,38 +18,42 @@ describe('RegisterForm component', () => {
     );
   });
 
-  it('should show error message if email format is not valid', async () => {
+  it('should show error message on blur if email format is not valid', async () => {
     const user = userEvent.setup();
     await user.type(screen.getByRole('textbox', { name: 'Email' }), 'badformat');
+    await user.tab();
 
     expect(screen.getByText('Invalid email')).toBeInTheDocument();
   });
 
-  it('should not show error message if email format is valid', async () => {
+  it('should not show error message on blur if email format is valid', async () => {
     const user = userEvent.setup();
     await user.type(screen.getByRole('textbox', { name: 'Email' }), 'correct@format.com');
+    await user.tab();
 
     expect(screen.queryByText('Invalid email')).not.toBeInTheDocument();
   });
 
-  it('should show error message if password is too short', async () => {
+  it('should show error message on blur if password is too short', async () => {
     const user = userEvent.setup();
     await user.type(screen.getByLabelText('Password'), 'short');
+    await user.tab();
 
     expect(
       screen.getByText(`Minimum password length is ${MIN_PASSWORD_LENGTH}`)
     ).toBeInTheDocument();
   });
 
-  it('should show error message if passwords do not match', async () => {
+  it('should show error message on blur if passwords do not match', async () => {
     const user = userEvent.setup();
     await user.type(screen.getByLabelText('Password'), 'password');
     await user.type(screen.getByLabelText('Repeat Password'), 'password2');
+    await user.tab();
 
     expect(screen.getByText("Passwords don't match")).toBeInTheDocument();
   });
 
-  it('should show error messages if email is already taken', async () => {
+  it('should show error message if email is already taken', async () => {
     const user = userEvent.setup();
     await user.type(screen.getByRole('textbox', { name: 'Email' }), mockUser.email);
     await user.type(screen.getByLabelText('Password'), mockUser.password);
--- src/pages/Register/components/RegisterForm/index.tsx
+++ src/pages/Register/components/RegisterForm/index.tsx
@@ -11,12 +11,14 @@ import {
 } from 'features/auth/authSlice';
 import { registerSchema, RegisterSchema } from 'features/auth/schemas/registerSchema';
 import Logo from 'components/common/Logo/Logo';
+import ErrorBox from 'components/ErrorBox';
 import Input from 'components/common/Input';
 import SubmitButton from 'components/common/SubmitButton';
 
 function RegisterForm() {
   const dispatch = useAppDispatch();
   const emailTaken = useAppSelector((state) => state.auth.error.emailTaken);
+  const serverError = useAppSelector((state) => state.auth.error.server);
   const isPendingAuth = useAppSelector(selectIsPendingAuth);
   const {
     register,
@@ -25,7 +27,7 @@ function RegisterForm() {
     clearErrors,
     formState: { errors },
   } = useForm<RegisterSchema>({
-    mode: 'onChange',
+    mode: 'onBlur',
     resolver: zodResolver(registerSchema),
   });
 
@@ -50,8 +52,13 @@ function RegisterForm() {
       className="mx-auto flex shrink basis-96 flex-col gap-4 rounded-sm bg-white p-6 drop-shadow-md"
       onSubmit={handleSubmit(onSubmit)}
     >
-      <header className="flex items-center justify-center">
+      <header className="flex flex-col items-center justify-center gap-4">
         <Logo />
+        <ErrorBox
+          isError={serverError}
+          title="Could not create an account"
+          errorMessage="Something went wrong. Please try again."
+        />
       </header>
       <Input label="Email" type="email" error={errors.email} {...register('email')} />
       <Input label="Password" type="password" error={errors.password} {...register('password')} />
