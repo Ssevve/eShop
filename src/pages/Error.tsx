8d9fea706e18d0d2e6d3d3c6661378b9e1c2a9fa
Seweryn Świrgoń <ssevve@gmail.com>
2023-06-25 15:56:09 +0200

    Remove unnecessary markup from Error and NotFound pages

--- src/pages/Error.tsx
+++ src/pages/Error.tsx
@@ -3,23 +3,19 @@ import Button from 'components/common/Button';
 
 function Error() {
   return (
-    <section className="bg-white">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
-        <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
-            Error!
-          </h1>
-          <p className="mb-4 text-lg font-light text-gray-500">
-            Could not get data from the server. Please try again later.
-          </p>
-          <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
-            <Button as={Link} to="/products">
-              Go to shop
-            </Button>
-            <Button as={Link} variant="neutral" to="/">
-              Go to home page
-            </Button>
-          </div>
+    <section className="mx-auto max-w-screen-2xl bg-white px-4 py-8 lg:px-6 lg:py-16">
+      <div className="text-center">
+        <h1 className="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">Error!</h1>
+        <p className="mb-4 text-lg font-light text-gray-500">
+          Could not get data from the server. Please try again later.
+        </p>
+        <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
+          <Button as={Link} to="/products">
+            Go to shop
+          </Button>
+          <Button as={Link} variant="neutral" to="/">
+            Go to home page
+          </Button>
         </div>
       </div>
     </section>
--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,27 +2,25 @@ import { Link } from 'react-router-dom';
 import Button from 'components/common/Button';
 function NotFound() {
   return (
-    <section className="bg-white">
-      <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
-        <div className="mx-auto max-w-screen-sm text-center">
-          <h1 className="mb-4 text-7xl font-extrabold tracking-tight text-primary lg:text-9xl">
-            404
-          </h1>
-          <p className="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
-            Page not found.
-          </p>
-          <p className="mb-4 text-lg font-light text-gray-500">Sorry, we can't find that page.</p>
-          <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
-            <Button as={Link} to="/products">
-              Go to shop
-            </Button>
-            <Button as={Link} variant="neutral" to="/">
-              Go to home page
-            </Button>
-          </div>
+    <div className="mx-auto max-w-screen-2xl bg-white px-4 py-8 lg:px-6 lg:py-16">
+      <div className="text-center">
+        <h1 className="mb-4 text-7xl font-extrabold tracking-tight text-primary lg:text-9xl">
+          404
+        </h1>
+        <p className="mb-4 text-3xl font-bold tracking-tight text-gray-900 md:text-4xl">
+          Page not found.
+        </p>
+        <p className="mb-4 text-lg font-light text-gray-500">Sorry, we can't find that page.</p>
+        <div className="flex flex-col items-center justify-center gap-4 md:flex-row">
+          <Button as={Link} to="/products">
+            Go to shop
+          </Button>
+          <Button as={Link} variant="neutral" to="/">
+            Go to home page
+          </Button>
         </div>
       </div>
-    </section>
+    </div>
   );
 }
 
