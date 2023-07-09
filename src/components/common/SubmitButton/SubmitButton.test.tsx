789131ea6eb160fe19b0c16a44e8e6956d37e706
Seweryn Świrgoń <ssevve@gmail.com>
2023-07-09 21:45:15 +0200

    Update SubmitButton tests

--- src/components/common/SubmitButton/SubmitButton.test.tsx
+++ src/components/common/SubmitButton/SubmitButton.test.tsx
@@ -4,29 +4,25 @@ import SubmitButton from '.';
 
 describe('SubmitButton component', () => {
   describe('when isLoading is false', () => {
-    const submitButton = <SubmitButton text="Log in" isLoading={false} />;
-
-    it('should render correct text', () => {
-      renderWithProviders(submitButton);
+    it('should render children', () => {
+      renderWithProviders(<SubmitButton isLoading={false}>Log in</SubmitButton>);
       expect(screen.getByRole('button', { name: /log in/i })).toBeInTheDocument();
     });
 
     it('should not be disabled', () => {
-      renderWithProviders(submitButton);
+      renderWithProviders(<SubmitButton isLoading={false}>Log in</SubmitButton>);
       expect(screen.getByRole('button')).not.toBeDisabled();
     });
   });
 
   describe('when isLoading is true', () => {
-    const submitButton = <SubmitButton text="Log in" isLoading />;
-
     it('should render loader', () => {
-      renderWithProviders(submitButton);
+      renderWithProviders(<SubmitButton isLoading>Log in</SubmitButton>);
       expect(screen.getByRole('status')).toBeInTheDocument();
     });
 
     it('should be disabled', () => {
-      renderWithProviders(submitButton);
+      renderWithProviders(<SubmitButton isLoading>Log in</SubmitButton>);
       expect(screen.getByRole('button')).toBeDisabled();
     });
   });
