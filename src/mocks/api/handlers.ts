7c4604091139a0892e73ad684bf4ea919b851c1c
Seweryn Świrgoń <ssevve@gmail.com>
2023-07-09 21:15:39 +0200

    Update RegisterForm tests

--- src/mocks/api/handlers.ts
+++ src/mocks/api/handlers.ts
@@ -1,11 +1,16 @@
 import { rest } from 'msw';
 import products from '../products';
+import mockUser from 'mocks/user';
 
 const handlers = [
   rest.get(`${import.meta.env.VITE_API_URL}/products/:id`, (req, res, ctx) => {
     const responseProduct = products.find((product) => product._id === req.params.id);
     if (!responseProduct) return res(ctx.status(404), ctx.json(null));
     return res(ctx.status(200), ctx.json(responseProduct));
+  }),
+  rest.post(`${import.meta.env.VITE_API_URL}/users/register`, async (req, res, ctx) => {
+    const { email } = await req.json();
+    if (email === mockUser.email) return res(ctx.status(409));
   }),
 ];
 
