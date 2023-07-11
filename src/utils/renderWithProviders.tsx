b46406ff440585927ef90852fc38d574f91199a2
Seweryn Świrgoń <ssevve@gmail.com>
2023-07-11 18:07:21 +0200

    Remove unnecessary comments from renderWithProviders function

--- src/utils/renderWithProviders.tsx
+++ src/utils/renderWithProviders.tsx
@@ -1,16 +1,12 @@
-// Custom render function from: https://redux.js.org/usage/writing-tests
+// DOCS: https://redux.js.org/usage/writing-tests
+
 import React, { PropsWithChildren } from 'react';
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { render } from '@testing-library/react';
-import type { RenderOptions } from '@testing-library/react';
-import type { PreloadedState } from '@reduxjs/toolkit';
+import { RenderOptions } from '@testing-library/react';
+import { PreloadedState } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-
 import { AppStore, RootState, setupStore } from '../app/store';
-// As a basic setup, import your same slice reducers
 
-// This type interface extends the default options for render from RTL, as well
-// as allows the user to specify other things such as initialState, store.
 interface ExtendedRenderOptions extends Omit<RenderOptions, 'queries'> {
   preloadedState?: PreloadedState<RootState>;
   store?: AppStore;
@@ -20,7 +16,6 @@ function renderWithProviders(
   ui: React.ReactElement,
   {
     preloadedState = {},
-    // Automatically create a store instance if no store was passed in
     store = setupStore(preloadedState),
     ...renderOptions
   }: ExtendedRenderOptions = {}
@@ -29,7 +24,6 @@ function renderWithProviders(
     return <Provider store={store}>{children}</Provider>;
   }
 
-  // Return an object with the store and all of RTL's query functions
   return { store, ...render(ui, { wrapper: Wrapper, ...renderOptions }) };
 }
 
