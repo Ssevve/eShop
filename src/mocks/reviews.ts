2e27ae9efa4c66f3c6f0cfd6da29cd1a7e0bc544
Seweryn Świrgoń <ssevve@gmail.com>
2023-07-12 20:50:45 +0200

    Mock success get reviews response

---
+++ src/mocks/reviews.ts
@@ -0,0 +1,30 @@
+import { Review } from 'app/services/reviews';
+
+const reviews: Review[] = [
+  {
+    rating: 1,
+    userFirstName: 'testUserFirstName1',
+    message: 'testReviewMessage1',
+    productId: 'testProductId',
+    userId: 'testUserId1',
+    _id: 'testReviewId1',
+  },
+  {
+    rating: 2,
+    userFirstName: 'testUserFirstName2',
+    message: 'testReviewMessage2',
+    productId: 'testProductId',
+    userId: 'testUserId2',
+    _id: 'testReviewId2',
+  },
+  {
+    rating: 3,
+    userFirstName: 'testUserFirstName3',
+    message: 'testReviewMessage3',
+    productId: 'testProductId',
+    userId: 'testUserId3',
+    _id: 'testReviewId3',
+  },
+];
+
+export default reviews;
