2ed069811268983a8eabb725d6d00b26e16cea30
Seweryn Świrgoń <ssevve@gmail.com>
2023-07-15 02:15:56 +0200

    Add danger-outline Button variant

--- src/features/users/UserProfile/index.tsx
+++ src/features/users/UserProfile/index.tsx
@@ -1,20 +1,21 @@
 import { useAppDispatch, useAppSelector } from 'app/hooks';
 import { logoutUser, selectCurrentUser } from 'features/auth/authSlice';
+import Input from 'components/common/Input';
+import Button from 'components/common/Button';
 
 function UserProfile() {
   const dispatch = useAppDispatch();
   const currentUser = useAppSelector(selectCurrentUser);
   return (
     <div>
-      <h1 className="text-4xl font-semibold">
-        {currentUser?.firstName} {currentUser?.lastName}
-      </h1>
-      <button
-        className="text-bold border bg-danger p-3 text-white"
-        onClick={() => dispatch(logoutUser())}
-      >
+      <form className="mb-6 max-w-xs space-y-3">
+        <Input type="text" label="First name" value={currentUser?.firstName} disabled />
+        <Input type="text" label="Last name" value={currentUser?.lastName} disabled />
+        <Input type="text" label="Email" value={currentUser?.email} disabled />
+      </form>
+      <Button onClick={() => dispatch(logoutUser())} variant="danger-outline">
         Log out
-      </button>
+      </Button>
     </div>
   );
 }
