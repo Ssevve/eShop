5e40a896c0bb891c0b0a8c2084f91ac68c298c83
Seweryn Świrgoń <ssevve@gmail.com>
2023-07-16 22:17:21 +0200

    Add Generic List component and replace ProductReviewList

---
+++ src/features/reviews/Review/ReviewAuthor/index.tsx
@@ -0,0 +1,25 @@
+import theme from 'lib/theme';
+import cherryAvatar from 'assets/avatar-cherry.svg';
+
+interface ReviewAuthorProps {
+  name: string;
+}
+
+function ReviewAuthor({ name }: ReviewAuthorProps) {
+  const avatarSize = theme.spacing[10];
+
+  return (
+    <>
+      <img
+        className="h-10 w-10 rounded-full"
+        width={avatarSize}
+        height={avatarSize}
+        src={cherryAvatar}
+        alt=""
+      />
+      <p className="font-medium">{name}</p>
+    </>
+  );
+}
+
+export default ReviewAuthor;
