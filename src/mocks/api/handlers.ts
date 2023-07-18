1e01fa929824abcb37beb2897d73a03c4c1d4659
Seweryn Świrgoń <ssevve@gmail.com>
2023-07-18 16:52:29 +0200

    Add EditableUserReview tests

--- src/mocks/api/handlers.ts
+++ src/mocks/api/handlers.ts
@@ -1,20 +1,21 @@
 import { rest } from 'msw';
-import products from '../products';
-import mockUser from 'mocks/user';
-import mockReviews from 'mocks/reviews';
+import productsMock from '../productsMock';
+import { userWithoutReviewMock as userMock } from 'mocks/userMock';
+import reviewsMock from 'mocks/reviewsMock';
 
 const handlers = [
   rest.get(`${import.meta.env.VITE_API_URL}/products/:id`, (req, res, ctx) => {
-    const responseProduct = products.find((product) => product._id === req.params.id);
+    const responseProduct = productsMock.find((product) => product._id === req.params.id);
     if (!responseProduct) return res(ctx.status(404), ctx.json(null));
     return res(ctx.status(200), ctx.json(responseProduct));
   }),
   rest.post(`${import.meta.env.VITE_API_URL}/users/register`, async (req, res, ctx) => {
     const { email } = await req.json();
-    if (email === mockUser.email) return res(ctx.status(409));
+    if (email === userMock.email) return res(ctx.status(409));
   }),
   rest.get(`${import.meta.env.VITE_API_URL}/reviews/product/:id`, (req, res, ctx) => {
-    return res(ctx.status(200), ctx.json(mockReviews));
+    const responseReviews = reviewsMock.filter((review) => review.productId === req.params.id);
+    return res(ctx.status(200), ctx.json(responseReviews));
   }),
 ];
 
