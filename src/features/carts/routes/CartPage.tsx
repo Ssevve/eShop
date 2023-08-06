38a33e1150ecacb7cd73eb80f2d5a4360118d0ef
Seweryn Świrgoń <ssevve@gmail.com>
2023-08-06 13:47:16 +0200

    Rename isLoading variable on cart page

--- src/features/carts/routes/CartPage.tsx
+++ src/features/carts/routes/CartPage.tsx
@@ -1,24 +1,24 @@
 import { Button } from '@/components/common/Button';
+import { Loader } from '@/components/common/Loader';
 import { CartProductList, CartSummary } from '@/features/carts';
 import { cartsApi } from '../api';
-import { Loader } from '@/components/common/Loader';
 
 export function CartPage() {
-  const { data: cart, isFetching, isUninitialized } = cartsApi.endpoints.getCart.useQueryState();
+  const { data: cart, isLoading, isUninitialized } = cartsApi.endpoints.getCart.useQueryState();
 
-  const isLoading = isFetching || isUninitialized;
+  const isLoadingData = isLoading || isUninitialized;
 
   return (
     <section className="mx-auto mb-auto flex w-full flex-col justify-center gap-4 self-start lg:flex-row">
       <section className="w-full lg:w-3/4">
         <header className="flex items-center justify-between border-b pb-6 pt-3">
           <h1 className="text-2xl font-bold">{`Cart (${cart?.totalProductAmount || 0})`}</h1>
-          <Button variant="neutral" disabled={isLoading}>
+          <Button variant="neutral" disabled={isLoadingData}>
             Clear cart
           </Button>
         </header>
         <div className="mt-4">
-          {isLoading ? <Loader /> : <CartProductList products={cart?.products} />}
+          {isLoadingData ? <Loader /> : <CartProductList products={cart?.products} />}
         </div>
       </section>
       <CartSummary />
