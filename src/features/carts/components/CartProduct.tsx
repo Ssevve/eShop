9277425afb5f5e12cb42818824dac3f0e54cd7e0
Seweryn Świrgoń <ssevve@gmail.com>
2023-08-06 13:47:57 +0200

    Pass just a productId to CartProduct component instead of a whole object

--- src/features/carts/components/CartProduct.tsx
+++ src/features/carts/components/CartProduct.tsx
@@ -1,39 +1,52 @@
+import { CartProductControls } from '@/components/common/CartProductControls';
 import { PriceGroup } from '@/components/common/PriceGroup';
 import theme from '@/lib/theme';
 import { Link } from 'react-router-dom';
-import { CartProduct as CartProductType } from '../types';
-import { CartProductControls } from './CartProductControls';
+import { cartsApi } from '../api';
 
 interface CartProductProps {
-  product: CartProductType;
-  verticalInput?: boolean;
+  productId: string;
 }
 
-export function CartProduct({ product, verticalInput }: CartProductProps) {
+export function CartProduct({ productId }: CartProductProps) {
+  const { cartId, cartProduct, isFetching } = cartsApi.endpoints.getCart.useQueryState(undefined, {
+    selectFromResult: ({ data, isFetching }) => ({
+      cartId: data?._id || '',
+      cartProduct: data?.products.find((cartProduct) => cartProduct.product._id === productId),
+      isFetching,
+    }),
+  });
   const imageHeight = theme.spacing[40];
 
-  return (
+  return cartProduct ? (
     <div className="grid grid-cols-2 place-items-center gap-3 py-3 sm:grid-cols-3">
       <Link
         className="col-span-2 grid w-full grid-cols-2 place-items-center gap-3 hover:underline sm:col-span-1 sm:justify-items-start"
-        to={`/products/${product?.product._id}`}
+        to={`/products/${cartProduct.product._id}`}
       >
         <img
           className="col-span-1 mx-auto block h-40 object-scale-down"
           height={imageHeight}
-          src={product.product.imageUrl}
-          alt={product.product.name}
+          src={cartProduct.product.imageUrl}
+          alt={cartProduct.product.name}
         />
-        <span className="col-span-1">{product.product.name}</span>
+        <span className="col-span-1">{cartProduct.product.name}</span>
       </Link>
       <section className="row-start-2 flex items-end sm:col-start-2 sm:row-start-1">
-        <PriceGroup price={product.product.price} discountPrice={product.product.discountPrice} />
+        <PriceGroup
+          price={cartProduct.product.price}
+          discountPrice={cartProduct.product.discountPrice}
+        />
       </section>
-      <CartProductControls
-        verticalInput={verticalInput}
-        productId={product.product._id}
-        initialAmount={product.amount}
-      />
+      <section className="sm:justify-self-end">
+        <CartProductControls
+          cartId={cartId}
+          productId={cartProduct.product._id}
+          productName={cartProduct.product.name}
+          productAmount={cartProduct.amount}
+          isFetchingCart={isFetching}
+        />
+      </section>
     </div>
-  );
+  ) : null;
 }
--- src/features/carts/components/CartProductList.tsx
+++ src/features/carts/components/CartProductList.tsx
@@ -1,6 +1,6 @@
 import { List } from '@/components/common/List';
-import useBreakpointValue from '@/hooks/useBreakpointValue';
-import useWindowWidth from '@/hooks/useWindowWidth';
+// import useBreakpointValue from '@/hooks/useBreakpointValue';
+// import useWindowWidth from '@/hooks/useWindowWidth';
 import { CartProduct as CartProductType } from '../types';
 import { CartProduct } from './CartProduct';
 
@@ -9,15 +9,15 @@ interface CartProductListProps {
 }
 
 export function CartProductList({ products }: CartProductListProps) {
-  const windowWidth = useWindowWidth();
-  const smallBreakpoint = useBreakpointValue('xs');
-  const isSmallWindowSize = windowWidth < smallBreakpoint;
+  // const windowWidth = useWindowWidth();
+  // const smallBreakpoint = useBreakpointValue('xs');
+  // const isSmallWindowSize = windowWidth < smallBreakpoint;
 
   return (
     <List
       items={products}
       getKey={({ product }) => product._id}
-      renderItem={(product) => <CartProduct product={product} verticalInput={isSmallWindowSize} />}
+      renderItem={(product) => <CartProduct productId={product.product._id} />}
       className="divide-y"
       emptyItemsMessage="Your cart is empty!"
       emptyItemsMessageClass="w-full py-12 text-center text-5xl font-bold text-gray-200 md:text-6xl"
