5af02dc619b73d16b5c8db2aadec38f3bc6f1594
Seweryn Świrgoń <ssevve@gmail.com>
2023-08-06 19:54:48 +0200

    Display cart totals

--- src/features/carts/components/CartSummary.tsx
+++ src/features/carts/components/CartSummary.tsx
@@ -1,27 +1,29 @@
-// import { useAppSelector } from '@/app/hooks';
 import { Button } from '@/components/common/Button';
+import { cartsApi } from '../api';
+import { Link } from 'react-router-dom';
+import { formatPrice } from '@/utils/format';
 
 export function CartSummary() {
-  // const originalPrice = useAppSelector(selectCartOriginalPrice);
-  // const orderTotal = useAppSelector(selectCartTotal);
-  // const discount = useAppSelector(selectCartDiscount);
+  const { data: cart } = cartsApi.endpoints.getCart.useQueryState();
   return (
     <section className="flex h-max w-full flex-col gap-3 justify-self-start bg-gray-100 p-3 lg:mt-3 lg:w-1/4">
       <div>
         <div className="flex justify-between">
           <span>Original price:</span>
-          {/* <span>{formatPrice(originalPrice)}</span> */}
+          <span>{formatPrice(cart?.originalPrice || 0)}</span>
         </div>
         <div className="flex justify-between text-danger">
           <span>Saved:</span>
-          {/* <span>{formatPrice(discount)}</span> */}
+          <span>{formatPrice(cart?.totalDiscount || 0)}</span>
         </div>
       </div>
       <div className="flex justify-between text-lg font-semibold">
         <span>Order total:</span>
-        {/* <span>{formatPrice(orderTotal)}</span> */}
+        <span>{formatPrice(cart?.finalPrice || 0)}</span>
       </div>
-      <Button fullWidth>Checkout</Button>
+      <Button renderAs={Link} to="/checkout" fullWidth>
+        Checkout
+      </Button>
     </section>
   );
 }
--- src/utils/calculate.ts
+++
@@ -1,9 +0,0 @@
-import { CartProduct } from "@/features/carts";
-
-export function calculateOriginalPrice(products: CartProduct[]) {
-  return products.reduce((total, entry) => total + entry.amount * entry.product.price, 0);
-}
-
-export function calculateCartTotal(products: CartProduct[]) {
-  return products.reduce((total, entry) => total + entry.amount * entry.product.discountPrice, 0);
-}
