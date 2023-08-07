832007e62a50f362c55a4c66a991977049da4d00
Seweryn Świrgoń <ssevve@gmail.com>
2023-08-07 10:54:06 +0200

    Select only needed data from the query result for the CartSummary

--- src/features/carts/components/CartSummary.tsx
+++ src/features/carts/components/CartSummary.tsx
@@ -4,22 +4,31 @@ import { Link } from 'react-router-dom';
 import { formatPrice } from '@/utils/format';
 
 export function CartSummary() {
-  const { data: cart } = cartsApi.endpoints.getCart.useQueryState();
+  const { originalPrice, totalDiscount, finalPrice } = cartsApi.endpoints.getCart.useQueryState(
+    undefined,
+    {
+      selectFromResult: ({ data }) => ({
+        originalPrice: data?.originalPrice,
+        totalDiscount: data?.totalDiscount,
+        finalPrice: data?.finalPrice,
+      }),
+    }
+  );
   return (
     <section className="sticky bottom-0 left-0 mb-8 h-max w-full justify-self-start border bg-white p-4 lg:right-0 lg:top-24 lg:w-1/4">
       <div>
         <div className="flex justify-between">
           <span>Original price:</span>
-          <span>{formatPrice(cart?.originalPrice || 0)}</span>
+          <span>{formatPrice(originalPrice || 0)}</span>
         </div>
         <div className="flex justify-between text-danger">
           <span>Saved:</span>
-          <span>{formatPrice(cart?.totalDiscount || 0)}</span>
+          <span>{formatPrice(totalDiscount || 0)}</span>
         </div>
       </div>
       <div className="my-4 flex justify-between text-lg font-semibold">
         <span>Order total:</span>
-        <span>{formatPrice(cart?.finalPrice || 0)}</span>
+        <span>{formatPrice(finalPrice || 0)}</span>
       </div>
       <Button renderAs={Link} to="/checkout" fullWidth>
         Checkout
