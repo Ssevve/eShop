7cef3d6c712ad1d8238219c7b18b6a22c424ad9f
Seweryn Świrgoń <ssevve@gmail.com>
2023-08-12 11:41:03 +0200

    Add ErrorPage tests

---
+++ src/routes/__tests__/ErrorPage.test.tsx
@@ -0,0 +1,42 @@
+import renderWithProviders from '@/utils/renderWithProviders';
+import { screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+import { ErrorPage } from '../ErrorPage';
+
+describe('ErrorPage', () => {
+  it('should render correct heading', () => {
+    renderWithProviders(
+      <BrowserRouter>
+        <ErrorPage />
+      </BrowserRouter>
+    );
+    expect(screen.getByRole('heading', { name: 'Error!' })).toBeInTheDocument();
+  });
+
+  it('should render correct paragraph', () => {
+    renderWithProviders(
+      <BrowserRouter>
+        <ErrorPage />
+      </BrowserRouter>
+    );
+    expect(screen.getByText('Something went wrong. Please try again.')).toBeInTheDocument();
+  });
+
+  it('should render shop link', () => {
+    renderWithProviders(
+      <BrowserRouter>
+        <ErrorPage />
+      </BrowserRouter>
+    );
+    expect(screen.getByRole('link', { name: /go to shop/i })).toBeInTheDocument();
+  });
+
+  it('should render home link', () => {
+    renderWithProviders(
+      <BrowserRouter>
+        <ErrorPage />
+      </BrowserRouter>
+    );
+    expect(screen.getByRole('link', { name: /go to home page/i })).toBeInTheDocument();
+  });
+});
