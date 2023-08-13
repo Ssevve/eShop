11b3fc9331b3a4142ad26a5f14fe760777f86b2c
Seweryn Świrgoń <ssevve@gmail.com>
2023-08-13 21:19:59 +0200

    Simplify Header logic; Use Backdrop component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,17 +1,16 @@
 import { useAppDispatch } from '@/app/hooks';
+import { Backdrop } from '@/components/common/Backdrop';
 import { Logo } from '@/components/common/Logo';
 import { logoutUser } from '@/features/auth';
 import { cartsApi } from '@/features/carts';
 import useBreakpointValue from '@/hooks/useBreakpointValue';
 import useWindowWidth from '@/hooks/useWindowWidth';
-import { AnimatePresence, motion } from 'framer-motion';
 import { useEffect, useState } from 'react';
 import { FiMenu, FiShoppingCart } from 'react-icons/fi';
 import { Link } from 'react-router-dom';
 import { DesktopMenu } from './DesktopMenu';
 import { MobileMenu } from './MobileMenu';
 import { UserDropdown } from './UserDropdown';
-import useScrollLock from './hooks/useScrollLock';
 
 export function Header() {
   const dispatch = useAppDispatch();
@@ -22,14 +21,14 @@ export function Header() {
   });
   const windowWidth = useWindowWidth();
   const largeBreakpoint = useBreakpointValue('lg');
-  const [, setIsScrollLocked] = useScrollLock();
   const [shouldShowMenu, setShouldShowMenu] = useState(false);
   const [shouldShowUserDropdown, setShouldShowUserDropdown] = useState(false);
 
   const isMobile = windowWidth < largeBreakpoint;
+  const isDesktop = windowWidth > largeBreakpoint;
 
   useEffect(() => {
-    toggleMenu(false);
+    setShouldShowMenu(false);
   }, [isMobile]);
 
   const logout = () => {
@@ -42,42 +41,30 @@ export function Header() {
     setShouldShowUserDropdown(bool);
   };
 
-  const toggleMenu = (bool?: boolean) => {
-    if (bool !== undefined) {
-      setShouldShowMenu(bool);
-      setIsScrollLocked(bool);
-    } else {
-      setShouldShowMenu((prev) => !prev);
-      setIsScrollLocked((prev) => !prev);
-    }
-  };
-
   return (
     <>
-      <AnimatePresence>
-        {shouldShowMenu && (
-          <motion.div
-            initial={{ opacity: 0 }}
-            animate={{ opacity: 1 }}
-            exit={{ opacity: 0 }}
-            className="fixed left-0 top-0 z-50 h-screen w-screen bg-black/60"
-          />
-        )}
-      </AnimatePresence>
-      <header className="sticky top-0 z-10 flex w-screen bg-white shadow lg:z-50">
+      {shouldShowMenu && <Backdrop />}
+      <header className="sticky top-0 z-10 flex w-full bg-white shadow lg:z-50">
         <nav className="mx-auto flex h-16 w-full max-w-screen-2xl items-center justify-between gap-3 px-3">
           {isMobile && (
-            <button aria-label="Show menu" type="button" onClick={() => toggleMenu()}>
+            <button
+              aria-label="Show menu"
+              type="button"
+              onClick={() => setShouldShowMenu((prev) => !prev)}
+            >
               <FiMenu size={24} />
             </button>
           )}
           <Link to="/">
             <Logo />
           </Link>
-          {!isMobile && (
-            <DesktopMenu shouldShowCategories={shouldShowMenu} toggleCategories={toggleMenu} />
+          {isDesktop && (
+            <DesktopMenu
+              shouldShowCategories={shouldShowMenu}
+              toggleCategories={setShouldShowMenu}
+            />
           )}
-          <div className="mr-6 flex justify-end gap-6">
+          <div className="flex justify-end gap-6">
             <UserDropdown
               logout={logout}
               isOpen={shouldShowUserDropdown}
@@ -101,7 +88,9 @@ export function Header() {
           </div>
         </nav>
       </header>
-      {isMobile && <MobileMenu toggleClose={() => toggleMenu(false)} isOpen={shouldShowMenu} />}
+      {isMobile && (
+        <MobileMenu toggleClose={() => setShouldShowMenu(false)} isOpen={shouldShowMenu} />
+      )}
     </>
   );
 }
