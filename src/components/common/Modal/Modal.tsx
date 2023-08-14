be1bc5076e8257a2f6f3a475bf299b61fe9cb59b
Seweryn Świrgoń <ssevve@gmail.com>
2023-08-14 11:00:52 +0200

    Change Modal padding

--- src/components/common/Modal/Modal.tsx
+++ src/components/common/Modal/Modal.tsx
@@ -20,12 +20,12 @@ export function Modal({ close, title, children }: ModalProps) {
         ref={modalRef}
         className="fixed left-1/2 top-1/2 z-50 max-h-[95%] w-[95%] max-w-xl -translate-x-1/2 -translate-y-1/2 overflow-y-auto overflow-x-hidden"
       >
-        <div className="relative flex h-full w-full flex-col justify-between rounded-sm bg-white px-4 py-8 shadow">
+        <div className="relative flex h-full w-full flex-col justify-between rounded-sm bg-white p-4 shadow">
           <header className="flex items-center justify-between">
             <h3 className="font-semibold uppercase text-gray-400">{title}</h3>
             <CloseMenuButton close={close} />
           </header>
-          <section className="mt-8 ">{children}</section>
+          <section className="mt-8">{children}</section>
         </div>
       </div>
     </div>
