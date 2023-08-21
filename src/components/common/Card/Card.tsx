8e66578073efd4800315fc4fc38d8838bd26c288
Seweryn Świrgoń <ssevve@gmail.com>
2023-08-22 01:02:09 +0200

    Add shadow to Card on hover

--- src/components/common/Card/Card.tsx
+++ src/components/common/Card/Card.tsx
@@ -8,7 +8,12 @@ interface CardProps {
 
 export function Card({ children, className }: CardProps) {
   return (
-    <div className={twMerge('rounded-sm border border-gray-200 bg-white p-4 shadow', className)}>
+    <div
+      className={twMerge(
+        'rounded-sm border border-gray-200 bg-white p-4 transition-shadow hover:shadow-md',
+        className
+      )}
+    >
       {children}
     </div>
   );
