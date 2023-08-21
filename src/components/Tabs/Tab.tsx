1a0145a1f73f743c3eefe5c36453cf5247808db9
Seweryn Świrgoń <ssevve@gmail.com>
2023-08-21 20:58:07 +0200

    Fix bad default tab paths on refresh

--- src/components/Tabs/Tab.tsx
+++ src/components/Tabs/Tab.tsx
@@ -6,11 +6,12 @@ interface TabProps {
 }
 
 export function Tab({ tab }: TabProps) {
+  const path = tab.default ? `/${window.location.pathname.split('/')[1]}` : tab.path;
   return (
     <li className="mr-1.5">
       <NavLink
         end
-        to={tab.default ? window.location.pathname : `${tab.path}`}
+        to={path}
         className={({ isActive }) =>
           `inline-block border-b-2 p-3 hover:border-gray-300 hover:text-gray-600 ${
             isActive ? 'border-primary text-primary' : 'border-transparent'
